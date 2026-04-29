f80433477206db0e53287e52b58f9ebfb5eaf13a
jiahaoqian <jiahaoqian@tencent.com>
2026-04-29 11:31:52 +0800

    feat(shell): wire AppShell via (app) and (marketing) group layouts

---
+++ app/(marketing)/layout.tsx
@@ -0,0 +1,5 @@
+import { AppShell } from "@/components/shell/app-shell";
+
+export default function MarketingLayout({ children }: { children: React.ReactNode }) {
+  return <AppShell>{children}</AppShell>;
+}
